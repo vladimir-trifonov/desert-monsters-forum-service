e01d0a9eba8e1a6d764545f46f1b87dcd03092e6
Viktor Chobanov <chobanov10100@mail.bg>
2016-11-27 20:58:04 +0200

    Delete categoryModel.js

--- post/categoryModel.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const mongoose = require('mongoose');
-
-var CategorySchema = new mongoose.Schema({
-  'name': String,
-});
-
-module.exports = mongoose.model('Category', CategorySchema);
